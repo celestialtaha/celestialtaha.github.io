0cde81f7d6153a18617862c7a1512d6c6feaeace
Taha <t.samavati@sishemi.ir>
2024-08-22 15:06:01 +0330

    update content

--- src/consts.ts
+++ src/consts.ts
@@ -1,16 +1,16 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "Taha Samavati",
+  EMAIL: "tahasamavati12@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NUM_WORKS_ON_HOMEPAGE: 3,
+  NUM_PROJECTS_ON_HOMEPAGE: 0,
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION: "Taha Samavati | personal blog and portfolio.",
 };
 
 export const BLOG: Metadata = {
@@ -31,14 +31,14 @@ export const PROJECTS: Metadata = {
 export const SOCIALS: Socials = [
   { 
     NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
+    HREF: "https://x.com/taha_iusm",
   },
   { 
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/celestialtaha"
   },
   { 
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    HREF: "https://www.linkedin.com/in/taha-samavati/",
   }
 ];
---
+++ src/content/work/binj.md
@@ -0,0 +1,8 @@
+---
+company: "Binj"
+role: "Data Scientist and Generative AI Engineer"
+dateStart: "01/06/2021"
+dateEnd: "Present"
+---
+
+abcd
